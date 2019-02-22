3b24bb9f4ed5799daad558bc789d7dc29aa2551a
Porter Savage <theportersavage@gmail.com>
2019-02-22 12:03:56 -0800

    create index for wordcounter view and route that connects it to the home view index

---
+++ WordCounter/Controllers/WordCounterController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using WordCounter.Models;
+
+namespace WordCounter.Controllers
+{
+    public class WordCounterController : Controller
+    {
+
+        [HttpGet("/match")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/match/new")]
+        public ActionResult New()
+        {
+            return View();
+        }
+    }
+}
--- WordCounter/Startup.cs
+++ WordCounter/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace PlacesYouveBeen
+namespace WordCounter
 {
     public class Startup
     {
