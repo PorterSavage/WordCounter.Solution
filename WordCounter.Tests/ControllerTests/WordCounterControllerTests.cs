0b9a5949f2fdebd8f3f3774264ef626445e683c6
Porter Savage <theportersavage@gmail.com>
2019-02-22 15:48:34 -0800

    implement my 3 test methods and made sure all test methods are passing

---
+++ WordCounter.Tests/ControllerTests/WordCounterControllerTests.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WordCounter.Controllers;
+using WordCounter.Models;
+
+namespace WordCounter.Tests
+{
+    [TestClass]
+    public class WordCounterControllerTest
+    {
+        [TestMethod]
+        public void Index_ReturnsCorrectView_True()
+        {
+            WordCounterController controller = new WordCounterController();
+            ActionResult indexView = controller.Index();
+            Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void New_ReturnsCorrectView_True()
+        {
+            WordCounterController controller = new WordCounterController();
+            ActionResult indexView = controller.New();
+            Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+        }
+    }
+}
--- WordCounter.Tests/ModelTests/WordCounterTest.cs
+++ WordCounter.Tests/ModelTests/WordCounterTest.cs
@@ -10,21 +10,21 @@ namespace WordCounter.Tests
         [TestMethod]
         public void IsWord_ChecksIfUserEntersAString()
         {
-            WordCounterClass testWordCounterClass = new WordCounterClass();
+            WordCounterClass testWordCounterClass = new WordCounterClass("","",0);
             Assert.AreEqual(true, testWordCounterClass.IsUserInputAString("q"));
         }
 
         [TestMethod]
         public void IsSingleMatch_CheckIfWordMatchesWithSentence()
         {
-            WordCounterClass testWordCounterClass = new WordCounterClass();
+            WordCounterClass testWordCounterClass = new WordCounterClass("","",0);
             Assert.AreEqual(true, testWordCounterClass.DoesWordAndSentenceHaveSingleMatch("q", "q"));
         }
 
         [TestMethod]
         public void IsDoubleMatch_CheckIfWordHasTwoMatchesWithSentence()
         {
-            WordCounterClass testWordCounterClass = new WordCounterClass();
+            WordCounterClass testWordCounterClass = new WordCounterClass("","",0);
             Assert.AreEqual(true, testWordCounterClass.DoesWordAndSentenceHaveDoubleMatch("q", "q q"));
 
         }
@@ -32,17 +32,16 @@ namespace WordCounter.Tests
         [TestMethod]
         public void MatchCount_KeepsScoreOfHowManyMatchesBetweenWordAndSentence()
         {
-            int matchCount = 0;
             string word = "to";
             string sentence = "I want to complete this my way";
-            int result = new WordCounterClass().HowManyMatchesDoYouHave(word, sentence);
+            int result = new WordCounterClass("","",0).HowManyMatchesDoYouHave(word, sentence);
             Assert.AreEqual(1, result);
         }
 
         [TestMethod]
         public void IsMultiMatch_CheckIfWordHasMultipleMatchesWithSentence()
         {
-            WordCounterClass testWordCounterClass = new WordCounterClass();
+            WordCounterClass testWordCounterClass = new WordCounterClass("","",0);
             Assert.AreEqual(true, testWordCounterClass.DoesWordAndSentenceHaveMultipleMatches("q", "you are q and they are q and we are all q"));
         }
     }
